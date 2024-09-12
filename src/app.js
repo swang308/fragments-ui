b63717ffda552727178fb191cdf367b366499001
swang308 <swang308@myseneca.ca>
2024-09-12 18:31:36 -0400

    add authenticated request to gragments api server

--- src/app.js
+++ src/app.js
@@ -1,45 +1,55 @@
 // src/app.js
 
 import { Auth, getUser } from './auth';
+import { getUserFragments } from './api';
 
-async function init() {
-  // Get our UI elements
-  const userSection = document.querySelector('#user');
-  const loginBtn = document.querySelector('#login');
-  const logoutBtn = document.querySelector('#logout');
+// async function init() {
+//   // Get our UI elements
+//   const userSection = document.querySelector('#user');
+//   const loginBtn = document.querySelector('#login');
+//   const logoutBtn = document.querySelector('#logout');
 
-  // Wire up event handlers to deal with login and logout.
-  loginBtn.onclick = () => {
-    // Sign-in via the Amazon Cognito Hosted UI (requires redirects), see:
-    // https://docs.amplify.aws/lib/auth/advanced/q/platform/js/#identity-pool-federation
-    Auth.federatedSignIn();
-  };
-  logoutBtn.onclick = () => {
-    // Sign-out of the Amazon Cognito Hosted UI (requires redirects), see:
-    // https://docs.amplify.aws/lib/auth/emailpassword/q/platform/js/#sign-out
-    Auth.signOut();
-  };
+//   // Wire up event handlers to deal with login and logout.
+//   loginBtn.onclick = () => {
+//     // Sign-in via the Amazon Cognito Hosted UI (requires redirects), see:
+//     // https://docs.amplify.aws/lib/auth/advanced/q/platform/js/#identity-pool-federation
+//     Auth.federatedSignIn();
+//   };
+//   logoutBtn.onclick = () => {
+//     // Sign-out of the Amazon Cognito Hosted UI (requires redirects), see:
+//     // https://docs.amplify.aws/lib/auth/emailpassword/q/platform/js/#sign-out
+//     Auth.signOut();
+//   };
 
-  // See if we're signed in (i.e., we'll have a `user` object)
-  const user = await getUser();
-  if (!user) {
-    // Disable the Logout button
-    logoutBtn.disabled = true;
-    return;
-  }
+//   // See if we're signed in (i.e., we'll have a `user` object)
+//   const user = await getUser();
+//   if (!user) {
+//     // Disable the Logout button
+//     logoutBtn.disabled = true;
+//     return;
+//   }
 
-  // Log the user info for debugging purposes
-  console.log({ user });
+//   // Log the user info for debugging purposes
+//   console.log({ user });
 
-  // Update the UI to welcome the user
-  userSection.hidden = false;
+//   // Update the UI to welcome the user
+//   userSection.hidden = false;
 
-  // Show the user's username
-  userSection.querySelector('.username').innerText = user.username;
+//   // Show the user's username
+//   userSection.querySelector('.username').innerText = user.username;
 
-  // Disable the Login button
-  loginBtn.disabled = true;
-}
+//   // Disable the Login button
+//   loginBtn.disabled = true;
+// }
 
 // Wait for the DOM to be ready, then start the app
+
+async function init() {
+  const user = await getUser();
+  // Do an authenticated request to the fragments API server and log the result
+  const userFragments = await getUserFragments(user);
+
+  // TODO: later in the course, we will show all the user's fragments in the HTML...
+}
+
 addEventListener('DOMContentLoaded', init);
